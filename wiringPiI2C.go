f85624d4701ed9e3855dab00628b3b426ad5bb37
Carlos Rivero Aro <carlosriveroaro7@gmail.com>
2016-12-04 02:58:21 +0100

    fixing i2c calls and errors

--- wiringPiI2C.go
+++ wiringPiI2C.go
@@ -12,34 +12,34 @@ import "unsafe"
 
 type I2C int
 
-func I2CSetupInterface(device string, devID int) *I2C {
-	var _new *I2C
+func I2CSetupInterface(device string, devID int) I2C {
+	var _new I2C
 	dev := C.CString(device)
 	defer C.free(unsafe.Pointer(dev))
-	_new = *int(C.wiringPiI2CSetupInterface(dev, C.int(devID)))
+	_new = I2C(C.wiringPiI2CSetupInterface(dev, C.int(devID)))
 	return _new
 }
 
-func I2CSetup(devID int) *I2C {
-	return *int(C.wiringPiI2CSetup(C.int(devID)))
+func I2CSetup(devID int) I2C {
+	return I2C(C.wiringPiI2CSetup(C.int(devID)))
 }
 
-func (i2c *I2C) Read() int {
+func (i2c I2C) Read() int {
 	return int(C.wiringPiI2CRead(C.int(i2c)))
 }
-func (i2c *I2C) ReadReg8(reg int) int {
+func (i2c I2C) ReadReg8(reg int) int {
 	return int(C.wiringPiI2CReadReg8(C.int(i2c), C.int(reg)))
 }
-func (i2c *I2C) ReadReg16(reg int) int {
+func (i2c I2C) ReadReg16(reg int) int {
 	return int(C.wiringPiI2CReadReg16(C.int(i2c), C.int(reg)))
 }
 
-func (i2c *I2C) Write(data int) int {
+func (i2c I2C) Write(data int) int {
 	return int(C.wiringPiI2CWrite(C.int(i2c), C.int(data)))
 }
-func (i2c *I2C) WriteReg8(reg, data int) int {
+func (i2c I2C) WriteReg8(reg, data int) int {
 	return int(C.wiringPiI2CWriteReg8(C.int(i2c), C.int(data)))
 }
-func (i2c *I2C) WriteReg16(reg, data int) int {
+func (i2c I2C) WriteReg16(reg, data int) int {
 	return int(C.wiringPiI2CWriteReg16(C.int(i2c), C.int(data)))
 }
